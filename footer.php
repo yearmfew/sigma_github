c774675b1d09b71bcfc35e58af20af18252253d0
yearmfew <brlylmz23@gmail.com>
2018-05-07 16:10:44 +0300

    deneme

---
+++ footer.php
@@ -0,0 +1,125 @@
+  <!-- Footer Start -->
+        <footer id="rs-footer" class="rs-footer">
+            <div class="footer-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="about-widget">
+                                <img src="images/logo.png" alt="Footer Logo">
+                                <p>We create Premium Html Themes for more than three years. Our team goal is to reunite the elegance.</p>
+                                <p class="margin-remove">We create Unique and Easy To Use Flexible Html Themes With Advanced</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <h3 class="footer-title">RECENT POSTS</h3>
+                            <div class="recent-post-widget">
+                                <div class="post-item">
+                                    <div class="post-date">
+                                        <span>28</span>
+                                        <span>June</span>
+                                    </div>
+                                    <div class="post-desc">
+                                        <h3 class="post-title"><a href="blog-details.html">Welcome to Construction Theme</a></h3>
+                                        <span class="post-category">Design</span>
+                                    </div>
+                                </div>
+                                <div class="post-item">
+                                    <div class="post-date">
+                                        <span>28</span>
+                                        <span>June</span>
+                                    </div>
+                                    <div class="post-desc">
+                                        <h3 class="post-title"><a href="blog-details.html">Welcome to Construction Theme</a></h3>
+                                        <span class="post-category">Design</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <h3 class="footer-title">customer</h3>
+                            <ul class="sitemap-widget">
+                                <li class="active"><a href="about.html">About</a></li>
+                                <li><a href="services.html">Service</a></li>
+                                <li><a href="project.html">Project</a></li>
+                                <li><a href="blog.html">Blog</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3">
+                            <h3 class="footer-title">SUBSCRIBE NOW</h3>
+                            <p class="news-note">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In auctor sem ut ex tincidunt posuere. Donec in lorem eu justo luctus elementum. </p>
+                            <form class="footer-subscribe">
+                                <input type="text" class="form-input" placeholder="Enter Your Email Address">
+                                <button type="submit" class="form-button"></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6">
+                            <div class="copyright">
+                                <p>&copy; 2017 <a href="#">KeenIT Solution</a>. All Rights Reserved.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-8 col-sm-6">
+                            <div class="text-right ft-bottom-right">
+                                <div class="footer-bottom-share">
+                                    <ul>
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                                    </ul>
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End -->
+
+        <!-- start scrollUp  -->
+        <div id="scrollUp">
+            <i class="fa fa-angle-up"></i>
+        </div>
+
+        <!-- modernizr js -->
+        <script src="js/modernizr-2.8.3.min.js"></script>
+        <!-- jquery latest version -->
+        <script src="js/jquery.min.js"></script>
+        <!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- op nav js -->
+        <script src="js/jquery.nav.js"></script>
+        <!-- owl.carousel js -->
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/slick.min.js"></script>
+        <!-- isotope.pkgd.min js -->
+        <script src="js/isotope.pkgd.min.js"></script>
+        <!-- imagesloaded.pkgd.min js -->
+        <script src="js/imagesloaded.pkgd.min.js"></script>
+        <!-- wow js -->
+        <script src="js/wow.min.js"></script>
+        <!-- counter top js -->
+        <script src="js/waypoints.min.js"></script>
+        <script src="js/jquery.counterup.min.js"></script>
+        <!-- magnific popup -->
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <!-- rsmenu js -->
+        <script src="js/rsmenu-main.js"></script>
+        <!-- plugins js -->
+        <script src="js/plugins.js"></script>
+        <!-- contact form js -->
+        <script src="js/contact.form.js"></script>
+        <!-- quote popup form js -->
+        <script src="js/quote.form.js"></script>
+        <!-- moogle map js -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgC6ZapXdUzFdeQOFhdm_wucwlDMMQ8CQ"></script>
+        <!-- main js -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
